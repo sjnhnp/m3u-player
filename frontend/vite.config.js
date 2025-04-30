1a30311b01d9f2bb32319bac771f21469bf36e76
sjnhnp <18591921+sjnhnp@users.noreply.github.com>
2025-04-30 11:21:46 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: path.resolve(__dirname, '..'), // 设置根目录为项目根目录
   build: {
-    outDir: path.resolve(__dirname, '../build'),
+    outDir: path.resolve(__dirname, '../build'), // 构建输出目录为项目根目录下的 build 文件夹
     manifest: true,
     assetsInlineLimit: 0,
     cssCodeSplit: true,
